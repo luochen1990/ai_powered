ebf886123bc234aa8f37728fe65c1a6326d216f6
LuoChen <luochen1990@gmail.com>
2024-08-05 23:11:21 +0800

    improve: known_models

--- src/ai_powered/llm_adapter/known_models.py
+++ src/ai_powered/llm_adapter/known_models.py
@@ -58,6 +58,7 @@ KNOWN_PLATFORMS : list[KnownPlatform] = [
             KnownModel("gorilla-llm/gorilla-openfunctions-v2-gguf/gorilla-openfunctions-v2-q4_K_M.gguf", ALL_FEATURES),
             KnownModel("lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF/Meta-Llama-3-8B-Instruct-Q4_K_M.gguf", set()),
             KnownModel("deepseek-coder-v2", set()),
+            KnownModel("lmstudio-community/DeepSeek-Coder-V2-Lite-Instruct-GGUF", set()),
         ]
     ),
 ]
