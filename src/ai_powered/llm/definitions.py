a83b8c3a353653dcd8e8b7c22becd685a8c2ed46
LuoChen <luochen1990@gmail.com>
2024-08-07 16:04:07 +0800

    chore: remove unused comments

--- src/ai_powered/llm/definitions.py
+++ src/ai_powered/llm/definitions.py
@@ -3,15 +3,6 @@ from dataclasses import dataclass
 import enum
 from typing import Any, Optional
 
-'''
-    信息确定的过程:
-    首先是连接信息被确定,如BASE URL,API KEY等
-    然后是模型信息被确定,如模型名称，模型参数等
-    模型信息确定后,模型支持的特性随之被确定,如是否支持函数调用,是否支持JSON格式的返回值等
-    再然后是函数信息被确定,如函数签名，函数文档等
-    最终模拟函数执行时,需要参考函数信息和模型信息,以及连接信息,来确定函数执行的具体方式
-'''
-
 class ModelFeature (enum.Enum):
     '''
     Ollama Doc: https://ollama.fan/reference/openai/#supported-features
