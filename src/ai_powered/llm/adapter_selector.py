fc4ab1fb44fe55b6c2fad087d3654da167756558
LuoChen <luochen1990@gmail.com>
2024-08-07 16:39:46 +0800

    improve: adapter_selector now use structured_output_adapter

--- src/ai_powered/llm/adapter_selector.py
+++ src/ai_powered/llm/adapter_selector.py
@@ -1,16 +1,22 @@
 from ai_powered.llm.adapters.generic_adapter import GenericFunctionSimulator
 from ai_powered.llm.adapters.tools_adapter import ToolsFunctionSimulator
 from ai_powered.llm.adapters.chat_adapter import ChatFunctionSimulator
+from ai_powered.llm.adapters.structured_output_adapter import StructuredOutputFunctionSimulator
 from ai_powered.llm.definitions import ModelFeature
 
 
 class FunctionSimulatorSelector (GenericFunctionSimulator):
     ''' implementation of FunctionSimulator for OpenAI compatible models '''
 
-    _selected_function_simulator: GenericFunctionSimulator
+    _selected_impl: GenericFunctionSimulator
 
-    def select_function_simulator(self) -> GenericFunctionSimulator:
-        if ModelFeature.tools in self.model_features:
+    def _select_impl(self) -> GenericFunctionSimulator:
+        if ModelFeature.structured_outputs in self.model_features:
+            return StructuredOutputFunctionSimulator(
+                self.function_name, self.signature, self.docstring, self.parameters_schema, self.return_schema,
+                self.client, self.model_name, self.model_features, self.model_options
+            )
+        elif ModelFeature.tools in self.model_features:
             return ToolsFunctionSimulator(
                 self.function_name, self.signature, self.docstring, self.parameters_schema, self.return_schema,
                 self.client, self.model_name, self.model_features, self.model_options
@@ -23,7 +29,7 @@ class FunctionSimulatorSelector (GenericFunctionSimulator):
 
     def __post_init__(self):
         super().__post_init__()
-        self._selected_function_simulator = self.select_function_simulator()
+        self._selected_impl = self._select_impl()
 
     def query_model(self, arguments_json: str) -> str:
-        return self._selected_function_simulator.query_model(arguments_json)
+        return self._selected_impl.query_model(arguments_json)
--- src/ai_powered/llm/adapters/structured_output_adapter.py
+++ src/ai_powered/llm/adapters/structured_output_adapter.py
@@ -2,22 +2,21 @@ import openai
 from openai.types.chat.chat_completion_message import ChatCompletionMessage
 from openai.types.chat.completion_create_params import ResponseFormat
 from ai_powered.llm.adapters.generic_adapter import GenericFunctionSimulator
-
+#from ai_powered.llm.definitions import ModelFeature
 
 class StructuredOutputFunctionSimulator (GenericFunctionSimulator):
     ''' implementation of FunctionSimulator for OpenAI compatible models '''
 
     def _param_response_format_maker(self) -> ResponseFormat | openai.NotGiven:
         ''' to be overrided '''
-        json_schema_format = {
+        return {
             "type": "json_schema",
             "json_schema": {
                 "name": "math_response",
-                "strict": True,
+                #"strict": True if ModelFeature.strict_mode in self.model_features else None, #NOTE: https://platform.openai.com/docs/guides/structured-outputs/supported-schemas
                 "schema": self.return_schema,
             }
         }
-        return json_schema_format #type: ignore #TODO: update OpenAI SDK
 
     def _response_message_parser(self, response_message: ChatCompletionMessage) -> str:
         tool_calls = response_message.tool_calls
