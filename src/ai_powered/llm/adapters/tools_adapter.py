dd9962f67b3d0a8f32d139538c851e25737b6002
LuoChen <luochen1990@gmail.com>
2024-08-14 21:12:03 +0800

    add: tools_adapter support tool_choice not supported case

--- src/ai_powered/llm/adapters/tools_adapter.py
+++ src/ai_powered/llm/adapters/tools_adapter.py
@@ -4,6 +4,7 @@ from openai.types.chat.chat_completion_message import ChatCompletionMessage
 from openai.types.chat.chat_completion_tool_choice_option_param import ChatCompletionToolChoiceOptionParam
 from ai_powered.tool_call import ChatCompletionToolParam
 from ai_powered.llm.adapters.generic_adapter import GenericFunctionSimulator
+from ai_powered.utils.parse_message import extract_json_from_message
 
 class ToolsFunctionSimulator (GenericFunctionSimulator):
     ''' implementation of FunctionSimulator for OpenAI compatible models '''
@@ -30,5 +31,13 @@ class ToolsFunctionSimulator (GenericFunctionSimulator):
     def _response_message_parser(self, response_message: ChatCompletionMessage) -> str:
         tool_calls = response_message.tool_calls
 
-        assert tool_calls is not None
-        return tool_calls[0].function.arguments
+        if tool_calls is not None:
+            return tool_calls[0].function.arguments
+        else: # 兼容不支持 tool_choice 的情况, 比如 ollama
+            message = response_message.content
+            assert message is not None
+
+            json_str = extract_json_from_message(message)
+            assert json_str is not None
+
+            return json_str
