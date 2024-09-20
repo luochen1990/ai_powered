828b6c2830b685d57240b1ecb0e543624091d61a
LuoChen <luochen1990@gmail.com>
2024-09-20 22:00:53 +0800

    improve: flake.nix (add packages)

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs";
+    easy-sync.url = "github:luochen1990/easy-sync/master";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, easy-sync }:
   let
     supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     eachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
@@ -15,6 +16,35 @@
     });
   in
   {
+
+    packages = eachSystem ({pkgs, system, ...}: rec {
+      default = ai-powered;
+      ai-powered = let
+        pyproject = pkgs.lib.importTOML ./pyproject.toml;
+        meta = pyproject.tool.poetry;
+        pypkgs = pkgs.python3Packages;
+      in
+      pypkgs.buildPythonPackage {
+        pname = meta.name;
+        version = meta.version;
+        pyproject = true;
+        src = ./.;
+        buildInputs = [ pypkgs.poetry-core ];
+        dependencies = [
+          easy-sync.packages.${system}.easy-sync
+          pypkgs.msgspec
+          pypkgs.openai
+        ];
+        doCheck = true;
+        meta = with pkgs.lib; {
+          description = meta.description;
+          homepage = meta.repository;
+          license = licenses.asl20;
+          maintainers = with maintainers; [ luochen1990 ];
+        };
+      };
+    });
+
     devShells = eachSystem ({pkgs, python, ...}: rec {
       default = poetry;
 
