8a4ab2594d1edd5647a6349a407cdd3d3c810e46
LuoChen <luochen1990@gmail.com>
2025-06-04 08:22:02 +0800

    fix: typing issue

--- src/ai_powered/llm/connection.py
+++ src/ai_powered/llm/connection.py
@@ -4,7 +4,7 @@ from easy_sync import Waitable, sync_compatible
 import httpx
 import openai
 from openai.types.chat.chat_completion import ChatCompletion
-from ai_powered.utils.function_wraps import wraps_arguments_type
+from ai_powered.utils.function_wraps import wraps_method_arguments_type
 
 
 class LlmConnection:
@@ -12,7 +12,8 @@ class LlmConnection:
     async_client: openai.AsyncOpenAI
     base_url: str | httpx.URL | None
 
-    def __init__(self,
+    def __init__(
+        self,
         api_key: str | None = None,
         organization: str | None = None,
         project: str | None = None,
@@ -27,35 +28,35 @@ class LlmConnection:
         self.base_url = base_url
 
         self.sync_client = openai.OpenAI(
-            api_key=api_key,
-            organization=organization,
-            project=project,
-            base_url=base_url,
-            timeout=timeout,
-            max_retries=max_retries,
-            default_headers=default_headers,
-            default_query=default_query,
-            http_client=sync_http_client,
+            api_key = api_key,
+            organization = organization,
+            project = project,
+            base_url = base_url,
+            timeout = timeout,
+            max_retries = max_retries,
+            default_headers = default_headers,
+            default_query = default_query,
+            http_client = sync_http_client,
         )
 
         self.async_client = openai.AsyncOpenAI(
-            api_key=api_key,
-            organization=organization,
-            project=project,
-            base_url=base_url,
-            timeout=timeout,
-            max_retries=max_retries,
-            default_headers=default_headers,
-            default_query=default_query,
-            http_client=async_http_client,
+            api_key = api_key,
+            organization = organization,
+            project = project,
+            base_url = base_url,
+            timeout = timeout,
+            max_retries = max_retries,
+            default_headers = default_headers,
+            default_query = default_query,
+            http_client = async_http_client,
         )
 
-        async_fn = partial(self.async_client.chat.completions.create, stream=False)
-        sync_fn = partial(self.sync_client.chat.completions.create, stream=False)
+        async_fn = partial(self.async_client.chat.completions.create, stream = False)
+        sync_fn = partial(self.sync_client.chat.completions.create, stream = False)
 
-        f = sync_compatible(sync_fn=sync_fn)(async_fn) #type: ignore
+        f = sync_compatible(sync_fn = sync_fn)(async_fn)  #type: ignore
         self._chat_completions = f
 
-    @wraps_arguments_type(openai.AsyncOpenAI().chat.completions.create)
+    @wraps_method_arguments_type(openai.AsyncOpenAI().chat.completions.create)
     def chat_completions(self, *args: list[Any], **kwargs: dict[str, Any]) -> Waitable[ChatCompletion]:
         return self._chat_completions(*args, **kwargs)
--- src/ai_powered/utils/function_wraps.py
+++ src/ai_powered/utils/function_wraps.py
@@ -1,18 +1,44 @@
-from typing import Any, Callable, TypeVar
-from typing_extensions import ParamSpec
-
+from typing import Any, Callable, TypeVar, cast
+from typing_extensions import ParamSpec, Concatenate
 
 P = ParamSpec('P')
 R = TypeVar('R')
+S = TypeVar('S')
 
 
-def wraps(origin_fn: Callable[P, R]) -> Callable[ [Callable[..., Any]], Callable[P, R]]:
+def wraps(origin_fn: Callable[P, R]) -> Callable[[Callable[..., Any]], Callable[P, R]]:
+
     def wrapper(fn: Callable[..., Any]) -> Callable[P, R]:
-        return fn #type: ignore
+        return fn  #type: ignore
+
     return wrapper
 
 
-def wraps_arguments_type(origin_fn: Callable[P, Any]) -> Callable[ [Callable[..., R]], Callable[P, R]]:
-    def wrapper(fn: Callable[..., R]) -> Callable[P, R]:
-        return fn #type: ignore
+def wraps_method_arguments_type(origin_fn: Callable[P, Any]) -> Callable[[Callable[Concatenate[S, ...], R]], Callable[Concatenate[S, P], R]]:
+
+    def wrapper(fn: Callable[..., R]) -> Callable[Concatenate[S, P], R]:
+
+        @wraps(fn)
+        def inner(*args: P.args, **kwargs: P.kwargs) -> R:
+            return fn(*args, **kwargs)
+
+        return cast(Callable[Concatenate[S, P], R], inner)
+
     return wrapper
+
+
+class Add:
+
+    def add(self, x: int, y: int) -> int:
+        return x + y
+
+
+class A:
+
+    @wraps_method_arguments_type(Add().add)
+    def add(self, *args: list[Any], **kwargs: dict[str, Any]) -> int:
+        return 1
+
+
+a = A()
+r = a.add(1, 2)
